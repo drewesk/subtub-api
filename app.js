01f20af6ed126d03e78260b181a5b91ba2109069
username <drewesk@gmail.com>
2025-07-12 20:17:30 -0400

    adding other built-in Express.js middlewares

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 
 import connectDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+// add nifty middleware built into express
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // process sent data in a simple format
+app.use(cookieParser()); // reads cookies from user data/store data from incoming requests,
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to SubTub subscription tracker API!');
