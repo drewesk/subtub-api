06ef07ee44d04562f0a3c01e4f67e496c70604de
username <drewesk@gmail.com>
2025-07-13 18:58:44 -0400

    still troubleshooting

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -2,7 +2,6 @@ import { ADMIN_TOKEN } from '../config/env.js';
 
 const adminAuth = (req, res, next) => {
   try {
-    console.log('ADMIN_TOKEN:', ADMIN_TOKEN);
     const token = req.headers.authorization;
 
     if (!token || token !== `Bearer ${ADMIN_TOKEN}`) {
@@ -10,7 +9,6 @@ const adminAuth = (req, res, next) => {
     }
 
     next();
-
   } catch (error) {
     res.status(401).json({
       message: 'Unauthorized',
